886fb5467cbc489de7ed58ab3b1ef0fbedb9bce5
PIKSSL <purpledog120@gmail.com>
2023-02-13 13:42:12 +0100

    elso commit- basis

---
+++ src/modell/Auto.java
@@ -0,0 +1,20 @@
+
+package modell;
+
+public class Auto extends Jarmu {
+
+    private boolean defekt;
+    
+    public Auto(){
+        
+    }
+    
+    public void kereketCserel(){
+        
+    }
+    
+    public boolean halad(){
+        return false;
+    }
+       
+}
---
+++ src/modell/Jarmu.java
@@ -0,0 +1,24 @@
+
+package modell;
+
+public abstract class Jarmu {
+    private boolean beinditva;
+    private boolean uzemanyag;
+    private boolean megerkezett;
+
+    public void beindit(){
+        
+    }
+    
+    public void leallit(){
+        
+    }
+    
+    public boolean tankol(){
+        return false;
+    }
+    
+    public boolean halad(){
+        return false;
+    }
+}
