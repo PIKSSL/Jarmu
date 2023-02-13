ff42fa9cb70f8440c7915d6feb9e28e60a87413a
PIKSSL <purpledog120@gmail.com>
2023-02-13 14:46:58 +0100

    masodik commit- functions

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -6,15 +6,26 @@ public class Auto extends Jarmu {
     private boolean defekt;
     
     public Auto(){
-        
+        defekt = false;
     }
     
     public void kereketCserel(){
-        
+        if(defekt)
+            defekt=false;
     }
     
+    @Override
     public boolean halad(){
-        return false;
+        if(super.isBeinditva()){
+            int def = (int)(Math.random()*4)+1;
+            if (def == 1) {
+                defekt = true;
+            }
+            if(defekt || !super.isUzemanyag()){
+                return false;
+            }
+        }
+    return true;
     }
-       
+    
 }
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -7,18 +7,36 @@ public abstract class Jarmu {
     private boolean megerkezett;
 
     public void beindit(){
-        
-    }
+        if(!beinditva)
+        beinditva = true; 
+   }
     
     public void leallit(){
-        
+        if(beinditva)
+        beinditva = false; 
     }
-    
+
     public boolean tankol(){
+        if(!uzemanyag && !beinditva){
+            uzemanyag = true;
+            return true;
+        }
         return false;
     }
     
     public boolean halad(){
-        return false;
+        return uzemanyag && beinditva;
+    }
+    
+    public boolean isBeinditva() {
+        return beinditva;
+    }
+
+    public boolean isUzemanyag() {
+        return uzemanyag;
+    }
+
+    public boolean isMegerkezett() {
+        return megerkezett;
     }
 }
